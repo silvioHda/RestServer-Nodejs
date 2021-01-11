5f03ecb6a86306fc0f9bfbb48d8977188d1022b6
unknown <silvioignacio_01@hotmail.com>
2021-01-11 17:45:33 -0600

    segundo commit

--- server/server.js
+++ server/server.js
@@ -2,46 +2,25 @@ require('./config/config');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
-app.use(bodyParser.json()) 
+app.use(bodyParser.json()) ;
 
+app.use(require('./routes/usuario'));
 
-app.get('/usuario', function (req, res) {
-  res.json('get usuario')
+ mongoose.connect('mongodb://localhost:27017/cafe', (err,res)=>{
+      if (err) throw err;
+      
+      console.log('base de datos online');
 });
 
-app.post('/usuario', function (req, res) {
-    let body = req.body;
 
-    if (body.nombre===undefined) {
-      res.status(400).json({
-        ok:false,
-        mensaje:'un nuombre es necesario',
-      
-      });   
-    }else{
-
-    res.json({
-        persona:body
-    });
-  }
-  });
-
-  app.put('/usuario/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-  });
-
-  app.delete('/usuario', function (req, res) {
-    res.json('delete usuario')
-  });
- 
 app.listen(process.env.PORT, ()=>{
     console.log('escuchando el puerto: ',3000);
 }); 
+
